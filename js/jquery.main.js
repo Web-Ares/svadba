ac9afb57806e0d57a859bcd9cc92418bdc1718d6
petruninevgeniy <petruninevgeniy@gmial.com>
2016-02-29 09:04:19 +0200

    our-team

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -96,7 +96,10 @@ var Slider = function (obj) {
         //private methods
         var _initSlider = function () {
 
-                _slider = new Swiper( _sliderWrapper );
+                _slider = new Swiper( _sliderWrapper, {
+                    autoplay: 5000,
+                    speed: 500
+                } );
 
             },
             _init = function () {
